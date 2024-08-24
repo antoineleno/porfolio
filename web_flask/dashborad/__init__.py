5d11fbe5b6b23067d81c5d2d79af562839264c26
antoineleno <johndoe@example.com>
2024-08-25 05:02:30 +0800

    porfolio

--- web_flask/dashborad/__init__.py
+++ web_flask/dashborad/__init__.py
@@ -3,6 +3,7 @@
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/campus/admin')
+app_views = Blueprint('app_views', __name__, url_prefix='/campus/admin', template_folder='templates', static_folder='static')
+
 
 from dashborad.dashboard import *
